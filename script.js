df9144f2c3153e6558ef3e474f4ee580edb2ad31
MohdNihaal <abdulshihar2@gmail.com>
2024-11-05 12:36:46 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const authToken = "bq2sxTjyUyzIO7YWDmpxQm1T0DW3X87u";
-const baseUrl = "https://blr1.blynk.cloud/external/api/update?token=";
+  const baseUrl = "https://blr1.blynk.cloud/external/api/update?token=";
 
-const buttons = document.querySelectorAll('button');
+  const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => {
-  let isPressed = false; // Track the button's toggle state
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const virtualPin = button.dataset.pin;
+      const url = `${baseUrl}${authToken}&${virtualPin}=0`;
 
-  button.addEventListener('click', () => {
-    isPressed = !isPressed; // Toggle the state on each click
-    const virtualPin = button.dataset.pin;
-    const url = `${baseUrl}${authToken}&${virtualPin}=${isPressed ? 1 : 0}`; // Set URL based on state
-
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.text();
+        })
+        .then(data => {
+          console.log('Success:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
   });
-});
